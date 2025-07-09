f58efb5b2f5b5c489d03b9d97241ac6da74ac400
heyingrui <34918053@qq.com>
2025-07-09 17:17:11 +0800

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
-require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
