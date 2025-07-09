6e090c58d18b5401e1210b38fd376e0491796bea
heyingrui <34918053@qq.com>
2025-07-09 17:07:02 +0800

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 3000;
 
