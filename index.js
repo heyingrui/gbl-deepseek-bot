a688168f4f442c594983c549c4c983f9be824c20
heyingrui <34918053@qq.com>
2025-07-10 13:03:14 +0800

    index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./firebase-service-account.json"); 
 
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
