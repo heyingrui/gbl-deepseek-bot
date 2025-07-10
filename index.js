c82349d77b6e4bf0f4fe8e70ef802739351c78ef
heyingrui <34918053@qq.com>
2025-07-10 19:07:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const fetch = require("node-fetch");
+fetch = require("node-fetch");
 
 async function callDeepSeek(queryText, intentName) {
   try{
